3b6102ad137cd6f1c203db128b58ceaf22455019
mahesh <umamahesh.ch1995@gmail.com>
2026-05-05 09:26:57 -0400

    BruteForce/Sorting/HashSet used

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -35,7 +35,11 @@ class Hashing{
         return false;
     }
 }
-
+class HashingLength{
+    public boolean containsDuplicate(int[] nums){
+        return Arrays.stream(nums).distinct().count() < nums.length;
+    }
+}
 
 
 public class ContainsDuplicate {
